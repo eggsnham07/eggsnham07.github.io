ae2203272f394fe06e2e0660c1e7d0d74d84d629
eggsnham07 <eggsnham07@gmail.com>
2021-06-07 09:17:18 -0600

    Added New Files

--- docs/script/devconsole.js
+++ docs/script/devconsole.js
@@ -1,18 +1,29 @@
+const begginerJS = `setTimeout(function(){
+    alert("This is part of the Javascript below!")
+}, 1000)`
+    
+const loadJs = `try {
+    ${localStorage.getItem("SavedJS")}
+} catch(err) { 
+    console.error(err) 
+}`
+
 /*
     JavaScript IDE
 */
+
 console.error = function(e) {
-    $("#console").append(`Error: ${e}`)
+    $("#console").append(`<span class="error">Error: ${e}</span><br>`)
 }
 
 console.warn = function(w) {
-    $("#console").append(`Warning: ${w}`)
+    $("#console").append(`<span class="warning">Warning: ${w}</span><br>`)
 }
 
 console.info = function(i) {
-    $("#console").append(`Info: ${i}`)
+    $("#console").append(`<span>Info: ${i}</span><br>`)
 }
 
 console.log = function(log) {
-    $("#console").append(log)
+    $("#console").append(`<span>${log}</span> <br>`)
 }
