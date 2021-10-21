166bc2275cf348e148d73871bed0007d2e5ffa03
eggsnham07 <73562975+eggsnham07@users.noreply.github.com>
2021-10-21 11:45:40 -0600

    Update IDE.js

--- docs/script/IDE.js
+++ docs/script/IDE.js
@@ -1,22 +1,18 @@
-function load() {
-    var html = document.getElementById("html");
-    var css = document.getElementById("css");
-    var js = document.getElementById("js");
-    var code = document.getElementById("code").contentWindow.document;
-  
-    document.body.onkeyup = function() {
-      code.open();
-      code.writeln(
-        html.value +
-          "<style>" +
-          css.value +
-          "</style>" +
-          "<script>" +
-          js.value +
-          "</script>"
-      );
-      code.close();
-    };
-}
+var html = document.getElementById("html");
+var css = document.getElementById("css");
+var js = document.getElementById("js");
+var code = document.getElementById("code").contentWindow.document;
 
-load()
+document.body.onkeyup = function() {
+  code.open();
+  code.writeln(
+    html.value +
+      "<style>" +
+      css.value +
+      "</style>" +
+      "<script>" +
+      js.value +
+      "</script>"
+  );
+  code.close();
+};
